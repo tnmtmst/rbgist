57baf8d3cd13c9c015a2336fd8ca5f4642a3dc4a
tnmtmst <tnmtmst@users.noreply.github.com>
2015-03-09 02:29:20 +0900

    Add for display with color

--- rbgist.rb
+++ rbgist.rb
@@ -4,6 +4,15 @@ require 'net/https'
 require 'cgi'
 require 'json'
 
+def cprint(str, clr)
+  colors = {
+    reset: 0, red: 31, green: 32, yellow: 33
+  }
+  print "\e[#{colors[clr].to_s}m"
+  print str
+  print "\e[#{colors[:reset].to_s}m"
+end
+
 USER =  `git config --global github.user`
 TOKEN = `git config --global github.token`.gsub(/(\r\n|\r|\n)/, "")
 
@@ -20,7 +29,8 @@ end
 body = JSON.parse res.body
 
 body.each do |gist|
-  print "id: #{gist['id']}"
+  print "id: "
+  cprint "#{gist['id']}", :yellow
   puts " #{gist['description'] || gist['files'].keys.join(" ")} #{gist['public'] ? '' : '(secret)'}"
   gist['files'].each do |file|
     puts "  * #{file[0]}"
