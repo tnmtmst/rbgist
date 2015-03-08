ea5af2135bfc9550e98fd1b7091be5e8fb47e202
tnmtmst <tnmtmst@users.noreply.github.com>
2015-03-09 01:22:09 +0900

    Add script to show Gist list

---
+++ rbgist.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'net/https'
+require 'cgi'
+require 'json'
+
+USER =  `git config --global github.user`
+TOKEN = `git config --global github.token`.gsub(/(\r\n|\r|\n)/, "")
+
+url   = URI.parse('https://api.github.com/')
+
+https = Net::HTTP.new(url.host, url.port)
+https.use_ssl = true
+
+req = Net::HTTP::Get.new("/gists?access_token=#{CGI.escape(TOKEN)}")
+res = https.start do |http|
+  http.request(req)
+end
+
+body = JSON.parse res.body
+
+body.each do |gist|
+  print "id: #{gist['id']}"
+  puts " #{gist['description'] || gist['files'].keys.join(" ")} #{gist['public'] ? '' : '(secret)'}"
+  gist['files'].each do |file|
+    puts "  * #{file[0]}"
+  end
+end
