6bd0c9721ecebf4295ffb50343a23afb2e81a3f5
tnmtmst <tnmtmst@users.noreply.github.com>
2015-03-09 03:38:39 +0900

    Change parse to permute

--- rbgist.rb
+++ rbgist.rb
@@ -5,49 +5,58 @@ require 'cgi'
 require 'json'
 require 'optparse'
 
-def cprint(str, clr)
-  colors = {
-    reset: 0, red: 31, green: 32, yellow: 33
-  }
-  print "\e[#{colors[clr].to_s}m"
-  print str
-  print "\e[#{colors[:reset].to_s}m"
-end
+class Gist
+  API_URL = URI.parse('https://api.github.com/')
 
-USER =  `git config --global github.user`
-TOKEN = `git config --global github.token`.gsub(/(\r\n|\r|\n)/, "")
-
-def list_gists
-  url   = URI.parse('https://api.github.com/')
-
-  https = Net::HTTP.new(url.host, url.port)
-  https.use_ssl = true
-
-  req = Net::HTTP::Get.new("/gists?access_token=#{CGI.escape(TOKEN)}")
-  res = https.start do |http|
-    http.request(req)
+  def initialize
+    @user =  `git config --global github.user`
+    @token = `git config --global github.token`.gsub(/(\r\n|\r|\n)/, "")
   end
 
-  body = JSON.parse res.body
+  def list_gists(options = {})
+    https = Net::HTTP.new(API_URL.host, API_URL.port)
+    https.use_ssl = true
 
-  body.each do |gist|
-    print "id: "
-    cprint "#{gist['id']}", :yellow
-    puts " #{gist['description'] || gist['files'].keys.join(" ")} #{gist['public'] ? '' : '(secret)'}"
-    gist['files'].each do |file|
-      puts "  * #{file[0]}"
+    req = Net::HTTP::Get.new("/gists?access_token=#{CGI.escape(@token)}")
+    res = https.start do |http|
+      http.request(req)
+    end
+
+    body = JSON.parse res.body
+
+    body.each do |gist|
+      print "id: "
+      cprint "#{gist['id']}", :yellow
+      puts " #{gist['description'] || gist['files'].keys.join(" ")} #{gist['public'] ? '' : '(secret)'}"
+
+      unless options[:oneline]
+        gist['files'].each do |file|
+          puts "  * #{file[0]}"
+        end
+      end
     end
   end
+
+  def cprint(str, clr)
+    colors = {
+      reset: 0, red: 31, green: 32, yellow: 33
+    }
+    print "\e[#{colors[clr].to_s}m"
+    print str
+    print "\e[#{colors[:reset].to_s}m"
+  end
 end
+
+gist = Gist.new
 
 options={}
 OptionParser.new do |opt|
-  opt.on('-l',     'list') {|v| options[:list] = v}
-  opt.on('--list', 'list') {|v| options[:list] = v}
+  opt.on('-l', '--list', 'list') {|v| options[:list] = v}
+  opt.on('--oneline', 'oneline') {|v| options[:oneline] = v}
 
-  opt.parse!(ARGV)
+  opt.permute!(ARGV)
 end
 
 if options[:list]
-  list_gists
+  gist.list_gists options
 end
