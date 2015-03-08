6fbd6964eb9b19c7fe50534b8ea913d9591cbf49
tnmtmst <tnmtmst@users.noreply.github.com>
2015-03-09 03:07:23 +0900

    Can be specified arg

--- rbgist.rb
+++ rbgist.rb
@@ -3,6 +3,7 @@
 require 'net/https'
 require 'cgi'
 require 'json'
+require 'optparse'
 
 def cprint(str, clr)
   colors = {
@@ -16,23 +17,37 @@ end
 USER =  `git config --global github.user`
 TOKEN = `git config --global github.token`.gsub(/(\r\n|\r|\n)/, "")
 
-url   = URI.parse('https://api.github.com/')
+def list_gists
+  url   = URI.parse('https://api.github.com/')
 
-https = Net::HTTP.new(url.host, url.port)
-https.use_ssl = true
+  https = Net::HTTP.new(url.host, url.port)
+  https.use_ssl = true
 
-req = Net::HTTP::Get.new("/gists?access_token=#{CGI.escape(TOKEN)}")
-res = https.start do |http|
-  http.request(req)
+  req = Net::HTTP::Get.new("/gists?access_token=#{CGI.escape(TOKEN)}")
+  res = https.start do |http|
+    http.request(req)
+  end
+
+  body = JSON.parse res.body
+
+  body.each do |gist|
+    print "id: "
+    cprint "#{gist['id']}", :yellow
+    puts " #{gist['description'] || gist['files'].keys.join(" ")} #{gist['public'] ? '' : '(secret)'}"
+    gist['files'].each do |file|
+      puts "  * #{file[0]}"
+    end
+  end
 end
 
-body = JSON.parse res.body
+options={}
+OptionParser.new do |opt|
+  opt.on('-l',     'list') {|v| options[:list] = v}
+  opt.on('--list', 'list') {|v| options[:list] = v}
 
-body.each do |gist|
-  print "id: "
-  cprint "#{gist['id']}", :yellow
-  puts " #{gist['description'] || gist['files'].keys.join(" ")} #{gist['public'] ? '' : '(secret)'}"
-  gist['files'].each do |file|
-    puts "  * #{file[0]}"
-  end
+  opt.parse!(ARGV)
+end
+
+if options[:list]
+  list_gists
 end
